80895826c9b5e6f562a56fee98e583b3e9f0cda1
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2020-07-29 04:33:22 -0300

    Inserido swagger para a documentação das APIs

--- src/main/java/com/onlycoders/camadaadm/api/rest/UsuarioController.java
+++ src/main/java/com/onlycoders/camadaadm/api/rest/UsuarioController.java
@@ -4,10 +4,12 @@
  * Decrição: Recebe as requisições, delega os processamentos e cuida dos retornos para api de usuários
  * #########################################################################
  */
-package com.onlycoders.camadaadm.model.api.rest;
+package com.onlycoders.camadaadm.api.rest;
 
 import com.onlycoders.camadaadm.model.entity.Usuario;
 import com.onlycoders.camadaadm.model.repository.UsuarioRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@Api(value = "Usuarios")
 @RequestMapping("/usuarios")
 @RequiredArgsConstructor
 @CrossOrigin("*")
@@ -24,27 +27,30 @@ public class UsuarioController {
 
     private final UsuarioRepository repository;
 
-
+    @ApiOperation(value = "Cria novo usuário")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Usuario salvar(@RequestBody Usuario usuario){
         return repository.save(usuario);
     }
 
+    @ApiOperation(value = "Deleta usuário")
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deletar(@PathVariable Integer Id){
-        repository.deleteById(Id);
+    public void deletar(@PathVariable Integer id){
+        repository.deleteById(id);
     }
 
+    @ApiOperation(value = "Lista usuário")
     @GetMapping
     public List<Usuario> list(){
         return repository.findAll();
     }
 
-    @PatchMapping("{id}/muda-nome")
-    public void MudaSenha( @PathVariable Integer Id, @RequestBody String nome){
-        Optional<Usuario> usuario = repository.findById(Id);
+    @ApiOperation(value = "muda senha usuário")
+    @PatchMapping("{id}/muda-senha")
+    public void MudaSenha( @PathVariable Integer id, @RequestBody String nome){
+        Optional<Usuario> usuario = repository.findById(id);
         usuario.ifPresent( c -> {
             c.setNome(nome);
             repository.save(c);
